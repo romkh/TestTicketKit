d026483b8187d2a9249b0c068232b401d531869b
Roman <pp.krome@gmail.com>
2017-02-28 11:40:42 +0200

    init

---
+++ TestTicketKit/TestTicketKit.h
@@ -0,0 +1,19 @@
+//
+//  TestTicketKit.h
+//  TestTicketKit
+//
+//  Created by Roman on 2/28/17.
+//  Copyright © 2017 upgraded. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestTicketKit.
+FOUNDATION_EXPORT double TestTicketKitVersionNumber;
+
+//! Project version string for TestTicketKit.
+FOUNDATION_EXPORT const unsigned char TestTicketKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestTicketKit/PublicHeader.h>
+
+
